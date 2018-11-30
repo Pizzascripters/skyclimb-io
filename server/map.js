0197e8be0954001c3e3c017f4d930cf1b1004dbc
Nik Olson <pizzascripters@gmail.com>
2018-11-30 12:17:39 -0600

    The map is now a list of vertices

--- server/map.js
+++ server/map.js
@@ -1,36 +1,70 @@
-module.exports = function(Matter){
-  let Vertices = Matter.Vertices,
+const Matter = require('./matter');
+
+const Vertices = Matter.Vertices,
       Bodies = Matter.Bodies,
       Body = Matter.Body,
       rectangle = Bodies.rectangle,
       circle = Bodies.circle,
       trapezoid = Bodies.trapezoid;
 
+module.exports = () => {
+
   let map = [];
+  let vertices = [];
 
-  // Outside box
-  map[0] = rectangle(0, 1000, 1880, 100);
-  map[1] = rectangle(1000, 0, 100, 1880);
-  map[2] = rectangle(-1000, 0, 100, 1880);
-  map[3] = rectangle(0, -1000, 1880, 100);
+  vertices.push( {x:-200, y:300} );
+  vertices.push( {x:100, y:300} );
+  vertices.push( {x:200, y:360} );
+  vertices.push( {x:300, y:340} );
 
-  map[4] = rectangle(1000, 1000, 100, 100);
-  map[5] = rectangle(1000, -1000, 100, 100);
-  map[6] = rectangle(-1000, 1000, 100, 100);
-  map[7] = rectangle(-1000, -1000, 100, 100);
-
-  map[8] = trapezoid(0, 300, 1000, 200, 0.8);
-  map[9] = trapezoid(0, 650, 555, 200, -0.8);
-  map[10] = rectangle(-500, -125, 50, 800);
-  map[11] = rectangle(-410, -50, 100, 50)
-  map[12] = circle(-100, -225, 75);
-  map[13] = rectangle(-625, -475, 175, 100)
-
-  /*body = Body.create({"position": vector});
-  Body.setVertices(body, array_of_vectors);*/
-
-  for(var i in map)
-    Body.setStatic(map[i], true);
+  let n = 0;
+  while(n < vertices.length - 1) {
+    if(n === vertices.length - 2 || findAngle(vertices, n+1) >= Math.PI / 2) {
+      const m = {
+        x: (vertices[n+1].x + vertices[n].x) / 2,
+        y: (vertices[n+1].y + vertices[n].y) / 2
+      };
+      const p = {
+        x: 2 * m.y - vertices[n+1].y,
+        y: 2 * m.x - vertices[n+1].x
+      };
+      createTriangle(map, vertices[n], vertices[n+1], p);
+      n++;
+    } else {
+      createTriangle(map, vertices[n], vertices[n+1], vertices[n+2]);
+      n += 2;
+    }
+  }
 
   return map;
 };
+
+function createTriangle(map, p1, p2, p3) {
+  const sortedVertices = Vertices.clockwiseSort([p1, p2, p3]);
+  body = Body.create({
+    position: Vertices.centre(sortedVertices),
+    isStatic: true
+  });
+  Body.setVertices(body, sortedVertices);
+  map.push(body);
+}
+
+function findAngle(vertices, i) {
+  const p1 = vertices[i-1];
+  const p2 = vertices[i];
+  const p3 = vertices[i+1];
+
+  return Math.acos(
+    Math.pow(distance(p1, p2), 2) +
+    Math.pow(distance(p1, p3), 2) +
+    Math.pow(distance(p2, p3), 2) /
+    (2 * distance(p1, p2) * distance(p1, p3))
+  );
+}
+
+function distance(p1, p2) {
+  return Math.sqrt(
+    Math.pow(p2.y - p1.y, 2) +
+    Math.pow(p2.x - p1.x, 2)
+  );
+}
